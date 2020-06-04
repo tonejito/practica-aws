23845faff963f7ea3271834116e0bef97c92a01a
Andrés Hernández <andres@tonejito.org>
2020-06-04 01:56:52 -0500

    Unified SG rules: pass {in out} all

--- modules/project-scenario/main.tf
+++ modules/project-scenario/main.tf
@@ -50,6 +50,10 @@ variable "s3_website_content" {
   type = list(map(string))
 }
 
+# variable "sg_rules" {
+#   type = list(map(string))
+# }
+
 ################################################################################
 # https://www.terraform.io/docs/providers/aws/
 provider "aws" {
@@ -291,57 +295,27 @@ resource "aws_security_group" "allow_all" {
 # https://www.terraform.io/docs/providers/aws/r/security_group_rule.html
 # TODO: Create SG rules in array
 
-resource "aws_security_group_rule" "allow_all_ingress_icmp" {
+resource "aws_security_group_rule" "allow_all_ingress" {
+  # count = length(var.sg_rules)
+  description       = "pass in all"
   type              = "ingress"
-  protocol          = "icmp"
-  from_port         = -1
-  to_port           = -1
+  protocol          = "-1"
+  from_port         = "0"
+  to_port           = "0"
   cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.allow_all.id
 }
 
-resource "aws_security_group_rule" "allow_all_ingress_tcp" {
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_all.id
-}
-
-resource "aws_security_group_rule" "allow_all_ingress_udp" {
-  type              = "ingress"
-  protocol          = "udp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_all.id
-}
-
-resource "aws_security_group_rule" "allow_all_egress_icmp" {
+resource "aws_security_group_rule" "allow_all_egress" {
+  # count = length(var.sg_rules)
+  description       = "pass out all"
   type              = "egress"
-  protocol          = "icmp"
-  from_port         = -1
-  to_port           = -1
+  protocol          = "-1"
+  from_port         = "0"
+  to_port           = "0"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_all.id
-}
-
-resource "aws_security_group_rule" "allow_all_egress_tcp" {
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_all.id
-}
-
-resource "aws_security_group_rule" "allow_all_egress_udp" {
-  type              = "egress"
-  protocol          = "udp"
-  from_port         = 0
-  to_port           = 65535
-  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.allow_all.id
 }
 
