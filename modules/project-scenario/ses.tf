1fa44f28a4dbe3bf9a1f9d0a668ba0df846b35e3
Andrés Hernández <andres@tonejito.org>
2020-06-04 19:43:04 -0500

    terraform fmt

--- modules/project-scenario/ses.tf
+++ modules/project-scenario/ses.tf
@@ -13,7 +13,7 @@ resource "aws_route53_record" "ses_verification_record" {
   name    = "_amazonses.${aws_ses_domain_identity.ses_domain_identity.id}"
   type    = "TXT"
   ttl     = "600"
-  records = [ aws_ses_domain_identity.ses_domain_identity.verification_token ]
+  records = [aws_ses_domain_identity.ses_domain_identity.verification_token]
 }
 
 resource "aws_ses_domain_identity_verification" "ses_domain_identity_verification" {
@@ -30,7 +30,7 @@ resource "aws_ses_domain_dkim" "ses_domain_dkim" {
 }
 
 resource "aws_route53_record" "ses_dkim_verification_record" {
-  count   = 3  # SES returns 3 DKIM CNAMES for verification
+  count   = 3 # SES returns 3 DKIM CNAMES for verification
   zone_id = aws_route53_zone.dns_zone.zone_id
   name    = "${element(aws_ses_domain_dkim.ses_domain_dkim.dkim_tokens, count.index)}._domainkey.${var.dns_domain}"
   type    = "CNAME"
